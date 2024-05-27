2c2ca7363002ddfb067807a23043e77cfb0e62bf
ericchiao2020 <167136082+ericchiao2020@users.noreply.github.com>
2024-05-27 18:48:42 +1000

    Update environment.prod.ts
    Updated currency code from 'GBP' to 'USD' and changed localCode from 'en-GB' to 'en-US'

--- website/ui/src/environments/environment.prod.ts
+++ website/ui/src/environments/environment.prod.ts
@@ -20,13 +20,13 @@
 export const environment = {
   production: true,
   // This is the currency used for all products and tagging/conversions
-  currency: 'GBP',
+  currency: 'USD',
   // This is the ISO_639 language code and the ISO_3166-1 Alpha 2 country code.
   // https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
   // https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
   // This is used to determine the pricing number format. For example, 'en-GB'
   // will format a number as '1,000.53'
-  localCode: 'en-GB',
+  localCode: 'en-US',
   // The ID of the container in Google Tag Manager, e.g. GTM-XXXXXXXX
   gtmContainerId: 'GTM-XXXXXXXX',
   //The path the scripts are loaded from. Change for loading scripts from your server instead (https://developers.google.com/tag-platform/tag-manager/server-side/dependency-serving)
